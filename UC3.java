e7957e4543396d50728b8436a98b96fde5b87800
AroodKesh <ak3102@srmist.edu.in>
2026-02-22 19:06:58 +0530

    Update UC3.java

--- UC3.java
+++ UC3.java
@@ -1,21 +1,13 @@
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        // Row 1
+public class OOPSBannerApp
+{
+    public static void main(String[] args)
+    {
         System.out.println(String.join("", "*****", " *****", " ***** ", " ***** "));
-        // Row 2
         System.out.println(String.join("", "** ** ", "** ** ", "** ** ", " ** "));
-        // Row 3
         System.out.println(String.join("", "** ** ", "** ** ", "** ** ", "  *** "));
-        // Row 4
         System.out.println(String.join("", "** ** ", "** ** ", "***** ", "   *** "));
-        // Row 5
         System.out.println(String.join("", "** ** ", "** ** ", "** ", "       ** "));
-        // Row 6
         System.out.println(String.join("", "** ** ", "** ** ", "** ", "    ** **"));
-        // Row 7
         System.out.println(String.join("", "***** ", "***** ", "** ", "    ***** "));
     }
 }
-
-
-
