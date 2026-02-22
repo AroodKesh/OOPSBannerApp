7b8b15e7888a99b2c375f46eab6b29c6f2c2fcd8
AroodKesh <ak3102@srmist.edu.in>
2026-02-22 18:41:13 +0530

    Update UC3.java

--- UC3.java
+++ UC3.java
@@ -1,15 +1,5 @@
-/**
- * OOPSBannerApp1 UC3 OOPS Banner Application (Use Case 3)
- * * This class refactors the banner display to use String.join() for better 
- * memory efficiency and readability[cite: 7, 8].
- * * @author Developer [cite: 88]
- * @version 3.0 [cite: 90]
- */
 public class OOPSBannerApp1 {
     public static void main(String[] args) {
-        // Construct each line using String.join() for better readability and efficiency[cite: 116].
-        // Each line corresponds to a row in the banner for the letters O, O, P, S[cite: 118].
-
         // Row 1
         System.out.println(String.join("", "*****", " *****", " ***** ", " ***** "));
         
@@ -32,3 +22,4 @@ public class OOPSBannerApp1 {
         System.out.println(String.join("", "***** ", "***** ", "** ", "    ***** "));
     }
 }
+
